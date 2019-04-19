6e4bea7f28cbad7850ac42d88872e4df7fe2d1a9
Valentin Grégoire <gregoire.valentin@cropdesign.com>
2019-04-19 22:18:38 +0200

    Initial commit

---
+++ src/main/java/com/example/springbootdemo/controller/DemoController.java
@@ -0,0 +1,22 @@
+package com.example.springbootdemo.controller;
+
+import com.example.springbootdemo.service.DemoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController(value = "/demo")
+public class DemoController {
+
+    private final DemoService demoService;
+
+    @Autowired
+    public DemoController(DemoService demoService) {
+        this.demoService = demoService;
+    }
+
+    @GetMapping
+    public String sayHello() {
+        return this.demoService.sayHello();
+    }
+}
---
+++ src/main/java/com/example/springbootdemo/service/DemoService.java
@@ -0,0 +1,10 @@
+package com.example.springbootdemo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DemoService {
+    public String sayHello() {
+        return "Hello world!";
+    }
+}
